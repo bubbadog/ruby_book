a557a8a3bb19c8f257232fb5a71dbc6daa6ef59a
Bubbadog <justin.karbowski@gmail.com>
2018-02-08 17:59:26 -0800

    add the_basics_exercises

---
+++ the_basics/the_basics_exercises.rb
@@ -0,0 +1,29 @@
+# puts "John" + " Doe"
+
+=begin
+a = 1756
+puts a/1000
+puts (a%1000)/100
+puts (a%100)/10
+puts (a%10)
+
+
+movie_hash = {"Cars": 2004, "A Toy Story": 1992}
+puts movie_hash[:"Cars"]
+puts movie_hash[:"A Toy Story"]
+
+movie_array = [2004, 1992]
+puts movie_array[0]
+puts movie_array[1]
+=end
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts (3.0 * 3.0)
+puts (4.0 * 4.0)
+puts (5.0 * 5)
+
+# needed to put a curly brace instead of a bracket
